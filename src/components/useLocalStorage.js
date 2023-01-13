175cbb9eecf6dfc4947394da9c6d7c0e08fe2ee3
Saurabh <saurabhgawande2016@gmail.com>
2023-01-13 19:56:11 +0530

    Done

--- src/components/useLocalStorage.js
+++ src/components/useLocalStorage.js
@@ -2,7 +2,19 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 const useLocalStorage = (stateVar, value) => {
-  
+     const [storedValue, setStoredValue] = useState(() => {
+          const item = window.localStorage.getItem(stateVar);
+          return item ? JSON.parse(item) : value;
+        });
+      
+        // create a function that updates the value in local storage and updates the state
+        const setValue = (value) => {
+          setStoredValue(value);
+          window.localStorage.setItem(stateVar, JSON.stringify(value));
+        }
+      
+        // return the current value and the function to update it
+        return [storedValue, setValue];
 };
 
 export default useLocalStorage;
